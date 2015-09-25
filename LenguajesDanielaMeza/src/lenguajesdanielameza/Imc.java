7182bd11daf49a8762d76e561f32bdf3827f6a47
T-107 <T-107@PC245>
2015-09-24 21:41:19 -0500

    Clase de Jueves 17/09/15

--- LenguajesDanielaMeza/src/lenguajesdanielameza/Imc.java
+++ LenguajesDanielaMeza/src/lenguajesdanielameza/Imc.java
@@ -8,6 +8,12 @@ public class Imc {
     float peso;
     float altura;
     
+    public Imc (float p, float a){
+        peso=p;
+        altura=a;
+        
+    }
+     
     public Imc(){
         peso=65f;
         altura=1.65f;
@@ -29,10 +35,28 @@ public class Imc {
         s=(short)i;
         l=b;
         
+        
+        
     
     }
     public float calcular (){
 float resultadoImc=peso/(altura*altura);
 return resultadoImc;
+
+
+
+if (resultadoImc <=20);
+else System.out.println ("Te encuentras en la etapa de Anorexia");
+
+if (resultadoImc <=20 && resultadoImc >=25)
+else System.out.println ("Felicidades eres una persona Sana");
+
+if ( resultadoImc >=25 & resultadoImc <=30);
+else System.out.println ("Lo sentimos usted esta pasado de Tamales");
+
+if ( resultadoImc >=30);
+else System.out.println ("Usted es un Tinaco con Patas");
+
+
     }    
 }
